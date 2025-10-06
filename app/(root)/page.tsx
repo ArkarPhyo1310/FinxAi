c397c662cca05ed9607bff12f9db3a4e5a3430a4
ArkarPhyo <arkarphyo.akp1996@gmail.com>
2025-10-06 19:35:19 +0700

    [impl] Implemented header and navigations

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="flex flex-col home-wrapper h-screen">
+      Home
+      <Button className="theme-btn">Click Me!</Button>
+    </div>
+  );
+};
+
+export default Home;
